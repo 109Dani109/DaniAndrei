64f10a3f2ea4a0ec2d0e0007ef13b9bc0422e7ff
109Dani109 <87154894+109Dani109@users.noreply.github.com>
2025-10-20 15:35:30 +0300

    Add files via upload
    Adaugare fisiere Proiect 3

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="ro">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Catalog Mașini</title>
+  <link rel="stylesheet" href="styleIndex.css">
+</head>
+<body>
+
+  <!-- Bara de navigație -->
+  <nav class="navbar">
+    <div class="logo">DnXPark</div>
+    <ul class="nav-links">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="catalog.html">Catalog</a></li>
+      <li><a href="locatii.html">Locații</a></li>
+      <li><a href="contact.html">Contact</a></li>
+    </ul>
+    <button class="login-btn">Login</button>
+  </nav>
+
+  <header>
+    <h1>Catalog Mașini</h1>
+    <p>Galerie de mașini responsive</p>
+  </header>
+
+  
+
+  <section class="container">
+    <div class="card">
+      <img src="ImaginiCatalog/daciaSpring.jpg" alt="Mașină sport roșie">
+      <h3>Dacia Logan</h3>
+      <p class="price">€5,900</p>
+      <p>Un coupe sportiv cu performanță puternică și interior premium.</p>
+    </div>
+
+    <div class="card">
+      <img src="images/car2.jpg" alt="SUV gri">
+      <h3>Urban SUV</h3>
+      <p class="price">€28,450</p>
+      <p>Spațios, eficient la consum și excelent pentru familie.</p>
+    </div>
+
+    <div class="card">
+      <img src="images/car3.jpg" alt="Mașină electrică albastră">
+      <h3>Electra EV</h3>
+      <p class="price">€42,000</p>
+      <p>Autonomie mare, încărcare rapidă și design modern.</p>
+    </div>
+
+    <div class="card">
+      <img src="images/car4.jpg" alt="Mașină clasică albastră">
+      <h3>Clasic Touring</h3>
+      <p class="price">€19,750</p>
+      <p>Confortabilă și ideală pentru naveta zilnică.</p>
+    </div>
+  </section>
+</body>
+</html>
